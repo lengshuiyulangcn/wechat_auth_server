43792af7fd2da648ea593dfae1ae523295c7c4cf
gyorou <gyorou@tjjtds.me>
2017-04-01 13:40:49 +0900

    add more field to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
     def self.from_token_request request
-      code = request.params["code"]
-      return nil unless code
-      User.find_or_create_by_code(code)
+      User.find_or_create_by_wechat(request)
     end
 
-    def self.find_or_create_by_code(code)
+    def self.find_or_create_by_wechat(request)
+      code = request.params["code"]
+      nickname  = request.params["user_info"]["nickName"]
+      avatar_url = request.params["user_info"]["avatarUrl"]
+      return nil unless code
       wechat_request_url  = "https://api.weixin.qq.com/sns/jscode2session?appid=#{ENV["WECHAT_APP_ID"]}&secret=#{ENV["WECHAT_APP_SECRET"]}&js_code=#{code}&grant_type=authorization_code"
       response = HTTParty.post(wechat_request_url)
       open_id = JSON.load(response.body)["openid"]
       return nil unless open_id
       entity = self.find_by(open_id: open_id)
       unless self.find_by(open_id: open_id)
-        entity = self.create(open_id: open_id)
+        entity = self.create(open_id: open_id, nickname: nickname, avatar_url: avatar_url )
       end
       return entity
   end
